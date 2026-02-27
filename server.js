9cbf4978161e308e962a6efb3876e09d1aad7cc4
Suchel <suchel.dc@gmail.com>
2026-02-27 16:32:18 +0100

    Add forward slash on path

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "frontend/views"));
+app.set("views", path.join(__dirname, "/frontend/views"));
 
-app.use(express.static(path.join(__dirname, "frontend/public")));
+app.use(express.static(path.join(__dirname, "/frontend/public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
